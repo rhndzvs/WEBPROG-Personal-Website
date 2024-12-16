fd46ecad215827c9c534d4b8215b5c7404e23b9e
rhndzvs <rrballesteros2@student.apc.edu.ph>
2024-12-16 16:30:48 +0800

    Added another JavaScript function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,8 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Surprise!";
-  }
+  document.getElementById("demo").innerHTML = "Surprise!";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("section");
+  document.getElementById("demo2").innerHTML = "Let me introduce myself again." + element[0].innerHTML;
+}
